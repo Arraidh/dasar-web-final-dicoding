4b410ab087d4eafd061831d5b3df35ce6a6cc073
Arraidh <ari.darmika@gmail.com>
2023-03-05 13:33:59 +0700

    Web Responsiveness

---
+++ app.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
